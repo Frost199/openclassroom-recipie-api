4618e7a99729c41f47b35afab3d733ba9f912e26
Frost199 <emmaldini12@gmail.com>
2019-11-04 02:38:38 +0100

    created recipie model

--- app.js
+++ app.js
@@ -1,24 +1,121 @@
+// MONGO user: manny
+// MONGO password for user: 14M48nVzC5AbDwsm
+// MONGO Connection: mongodb+srv://manny:<password>@cluster0-dmszo.mongodb.net/test?retryWrites=true&w=majority
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Recipe = require('./model/recipe');
 
 const app = express();
 
+//connecting to database
+mongoose.connect('mongodb+srv://manny:14M48nVzC5AbDwsm@cluster0-dmszo.mongodb.net/test?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas!')
+    })
+    .catch((error) => {
+        console.log('Unable to connect to MongoDB Atlas!');
+        console.error(error);
+    });
+
+//avoiding CORS(Cross Origin Resource Sharing), this adds to the headed
 app.use((req, res, next) => {
-  console.log('Request received!');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type,' +
+        ' Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.use(bodyParser.json());
+
+//Create New thing
+app.post('/api/stuff', (req, res, next) => {
+    const recipe = new Recipe({
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    recipe.save()
+        .then(() => {
+            res.status(201).json({
+                message: "Post saved successfully"
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            });
+        });
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' });
-  next();
+//view all about a particular thing
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({
+        _id: req.params.id
+    }).then((thing) => {
+        res.status(200).json(thing)
+    }).catch(error => {
+        res.status(404).json({
+            error: error
+        })
+    });
 });
 
-app.use((req, res, next) => {
-  console.log('Response sent successfully!');
+//Update a particular thing
+app.put('/api/stuff/:id', (req, res, next) => {
+    const thing = new Thing({
+        _id: req.params.id,
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        userId: req.body.userId
+    });
+    Thing.updateOne({_id: req.params.id}, thing)
+        .then(() => {
+            res.status(201).json({
+                message: 'Thing updated successfully'
+            });
+        })
+        .catch(error => {
+            res.status(400).json({
+               error: error
+            });
+        });
+});
+
+//delete a particular thing
+app.delete('/api/stuff/:id', (req, res, next) => {
+   Thing.deleteOne({
+       _id: req.params.id
+   }).then(() => {
+       res.status(200).json({
+           message: 'Deleted!'
+       })
+   }).catch(error => {
+      res.status(400).json({
+         error: error
+      });
+   });
+});
+
+
+
+//View all things
+app.use('/api/stuff', (req, res, next) => {
+    Thing.find()
+        .then((things) => {
+            res.status(200).json(things)
+        })
+        .catch(error => {
+            res.status(400).json({
+                error: error
+            })
+        });
 });
 
 module.exports = app;
